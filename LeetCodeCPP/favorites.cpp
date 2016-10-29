d110e70acf4e0270b9fbeb4e2d59592afcabbbcc
theodore <njuzheqin@gmail.com>
2016-10-28 22:20:21 -0400

    Add two-end bfs solution for 126. WordLadder II

--- LeetCodeCPP/favorites.cpp
+++ LeetCodeCPP/favorites.cpp
@@ -1147,6 +1147,7 @@ public:
         vector<string> path {beginWord};
         dfs(beginWord, endWord, res, path, word_map);
         return res;
+        //-----------run time 325ms betas 63.21%-------------------
     }
 
     //recursively search for the path
@@ -1160,6 +1161,68 @@ public:
             path.push_back(neighbor);
             dfs(neighbor, endWord, res, path, word_map);
             path.pop_back();
+        }
+    }
+
+    //an alternative solution, using two end bfs
+    //from https://discuss.leetcode.com/topic/16826/88ms-accepted-c-solution-with-two-end-bfs-68ms-for-word-ladder-and-88ms-for-word-ladder-ii/3
+
+    vector<vector<string>> findLadders_perfect(string start, string end, unordered_set<string> &dict) {
+        vector<vector<string> > ladders;
+        vector<string> ladder;
+        ladder.push_back(start);
+        unordered_set<string> startWords, endWords;
+        startWords.insert(start);
+        endWords.insert(end);
+        unordered_map<string, vector<string> > children;
+        bool flip = true;
+        if (searchLadders(startWords, endWords, dict, children, flip))
+            genLadders(start, end, children, ladder, ladders);
+        return ladders;
+        //-------run time 96ms betas 79.12%-------------
+    }
+
+    bool searchLadders(unordered_set<string>& startWords, unordered_set<string>& endWords, 
+            unordered_set<string>& dict, unordered_map<string, vector<string> >& children, bool flip) {
+        flip = !flip;
+        if (startWords.empty()) return false;
+        if (startWords.size() > endWords.size())
+            return searchLadders(endWords, startWords, dict, children, flip);
+        for (string word : startWords) dict.erase(word);
+        for (string word : endWords) dict.erase(word);
+        unordered_set<string> intermediate;
+        bool done = false;
+        for (string word : startWords) {
+            int n = word.length();
+            string temp = word;
+            for (int p = 0; p < n; p++) {
+                char letter = word[p];
+                for (int i = 0; i < 26; i++) {
+                    word[p] = 'a' + i;
+                    if (endWords.find(word) != endWords.end()) {
+                        done = true;
+                        flip ? children[word].push_back(temp) : children[temp].push_back(word);
+                    }
+                    else if (!done && dict.find(word) != dict.end()) {
+                        intermediate.insert(word);
+                        flip ? children[word].push_back(temp) : children[temp].push_back(word);
+                    }
+                }   
+                word[p] = letter;
+            }
+        }
+        return done || searchLadders(endWords, intermediate, dict, children, flip);
+    }
+    void genLadders(string& start, string& end, unordered_map<string, vector<string> >& children, 
+            vector<string>& ladder, vector<vector<string> >& ladders) {
+        if (start == end) {
+            ladders.push_back(ladder);
+            return;
+        }
+        for (string child : children[start]) {
+            ladder.push_back(child);
+            genLadders(child, end, children, ladder, ladders);
+            ladder.pop_back();
         }
     }
     
